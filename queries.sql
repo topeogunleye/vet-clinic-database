3ae6720b80ec56dace0dcfa80bd7647a49437a1e
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-04-26 07:53:19 +0100

    Comment out table

--- queries.sql
+++ queries.sql
@@ -4,9 +4,15 @@ SELECT *
 FROM animals
 where name like '%mon';
 
- id |  name   | date_of_birth | escape_attempts | neutered | weight_kg 
-----+---------+---------------+-----------------+----------+-----------
-  1 | Agumon  | 2020-02-03    |              10 | t        |         0
-  2 | Gabumon | 2018-11-15    |               8 | t        |         2
-  4 | Devimon | 2017-05-12    |              11 | t        |         5
-(3 rows)
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg 
+-- ----+---------+---------------+-----------------+----------+-----------
+--   1 | Agumon  | 2020-02-03    |              10 | t        |         0
+--   2 | Gabumon | 2018-11-15    |               8 | t        |         2
+--   4 | Devimon | 2017-05-12    |              11 | t        |         5
+-- (3 rows)
+
+
+SELECT *
+FROM animals 
+WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
