50996ad93849935f5d41e02f212a0588efdfd712
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-04-26 09:08:12 +0100

    Correct data inserted in sql

--- data.sql
+++ data.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
     VALUES 
-        ('Agumon', '2020-02-03', 10.23, true, 0),
-        ('Gabumon', '2018-11-15', 8, true, 2),
-        ('Pikachu', '2021-01-07', 15.04, false, 1),
-        ('Devimon', '2017-05-12', 11, true, 5);
+        ('Agumon', '2020-02-03', 0, true, 10.23),
+        ('Gabumon', '2018-11-15', 2, true, 8),
+        ('Pikachu', '2021-01-07', 1, false, 13.04),
+        ('Devimon', '2017-05-12', 5, true, 11);
