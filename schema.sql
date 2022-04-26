ecb2746e707245f8afd275d58cc15ff555cde8f9
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-04-26 07:24:03 +0100

    Create animals table

--- schema.sql
+++ schema.sql
@@ -2,4 +2,13 @@
 -- CREATE DATABASE
 CREATE DATABASE vet_clinic;
 
+DROP TABLE IF EXISTS animals;
+CREATE TABLE animals (
+    id INT GENERATED ALWAYS AS IDENTITY
+    name VARCHAR(100)
+    date_of_birth DATE
+    escape_attempts INT
+    neutered BOOL
+    weight_kg DECIMAL
+);
 
