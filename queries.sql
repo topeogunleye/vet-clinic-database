88b1d9435f13f2f5113c8d5d6e4b9de6924659fd
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-04-26 07:55:00 +0100

    Select all the name of all animals born between 2016 and 2019.

--- queries.sql
+++ queries.sql
@@ -16,3 +16,8 @@ SELECT *
 FROM animals 
 WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg 
+-- ----+---------+---------------+-----------------+----------+-----------
+--   2 | Gabumon | 2018-11-15    |               8 | t        |         2
+--   4 | Devimon | 2017-05-12    |              11 | t        |         5
+-- (2 rows)
