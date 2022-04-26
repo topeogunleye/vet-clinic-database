51f032770a8484dea90e9006a7a203bcbb7b46c5
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-04-26 07:48:12 +0100

    Add commas to animals table

--- schema.sql
+++ schema.sql
@@ -1,14 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 -- CREATE DATABASE
-CREATE DATABASE vet_clinic;
+-- CREATE DATABASE vet_clinic;
 
 DROP TABLE IF EXISTS animals;
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY
-    name VARCHAR(100)
-    date_of_birth DATE
-    escape_attempts INT
-    neutered BOOL
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOL,
     weight_kg DECIMAL
 );
 
