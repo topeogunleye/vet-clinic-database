40acdab529d8fd7bc6711bc8a72f8afe8c6c4b54
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-04-26 07:49:32 +0100

    Select from animals where name ends with 'mon'

--- queries.sql
+++ queries.sql
@@ -1,3 +1,12 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT *
+FROM animals
+where name like '%mon';
+
+ id |  name   | date_of_birth | escape_attempts | neutered | weight_kg 
+----+---------+---------------+-----------------+----------+-----------
+  1 | Agumon  | 2020-02-03    |              10 | t        |         0
+  2 | Gabumon | 2018-11-15    |               8 | t        |         2
+  4 | Devimon | 2017-05-12    |              11 | t        |         5
+(3 rows)
