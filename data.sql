f4cbdfb51c096d1f569821e8c04e78414b30311e
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-04-27 12:13:50 +0100

    Move transactions back to data

--- data.sql
+++ data.sql
@@ -13,3 +13,79 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
         ('Boarmon', '2005-06-07', 7, true, 20.4),
         ('Blossom', '1998-10-13', 3, true, 17),
         ('Ditto', '2022-05-14', 4, true, 4);
+
+-- begin the transaction
+BEGIN;
+
+-- update the animals table by setting the species column to unspecified
+UPDATE animals
+SET species = 'unspecified';
+
+-- roll back the transaction
+ROLLBACK;
+
+-- begin the transaction
+BEGIN;
+
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+UPDATE animals
+SET species = 'digimon'
+WHERE name like '%mon';
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+---------+---------------+-----------------+----------+-----------+---------
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     13.04 | 
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon | 2018-11-15    |               2 | t        |         8 | digimon
+--   4 | Devimon | 2017-05-12    |               5 | t        |        11 | digimon
+-- (4 rows)
+
+
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+---------+---------------+-----------------+----------+-----------+---------
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon | 2018-11-15    |               2 | t        |         8 | digimon
+--   4 | Devimon | 2017-05-12    |               5 | t        |        11 | digimon
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     13.04 | pokemon
+-- (4 rows)
+
+-- commit the transaction
+COMMIT;
+
+-- Inside a transaction delete all records in the animals table, then roll back the transaction.
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+-- Inside a transaction: Delete all animals born after Jan 1st, 2022.
+BEGIN;
+DELETE
+FROM
+     animals
+WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint for the transaction.
+SAVEPOINT my_savepoint;
+
+-- Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals
+SET
+    weight_kg = weight_kg * -1;
+
+-- rollback the transaction to the savepoint
+ ROLLBACK TO SAVEPOINT my_savepoint;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals
+SET 
+    weight_kg = weight_kg * -1
+WHERE
+    weight_kg < 0;
+
+-- commit the transaction
+COMMIT;
