23f17d8ce42d4bc75e544f82df1685d3725a98a1
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-04-28 05:02:58 +0100

    Insert data into species table

--- data.sql
+++ data.sql
@@ -99,3 +99,8 @@ INSERT INTO owners (fullname, age)
         ('Melody Pond', 77),
         ('Dean Winchester', 14),
         ('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+    VALUES 
+        ('Pokemon'),
+        ('Digimon');
