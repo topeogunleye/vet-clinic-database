dc89c118d37c805a46c2cea277ce5963fa0244e6
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-04-28 04:53:30 +0100

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -89,3 +89,13 @@ WHERE
 
 -- commit the transaction
 COMMIT;
+
+
+INSERT INTO owners (fullname, age)
+    VALUES 
+        ('Sam Smith', 34),
+        ('Jennifer Orwell', 19),
+        ('Bob', 45),
+        ('Melody Pond', 77),
+        ('Dean Winchester', 14),
+        ('Jodie Whittaker', 38);
