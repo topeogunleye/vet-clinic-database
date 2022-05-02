e0035fad3bda3b25fcb80bdc06e1af02e01ca585
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-05-02 09:21:18 +0100

    Insert large amount data into database

--- data.sql
+++ data.sql
@@ -30,9 +30,9 @@ BEGIN;
 UPDATE animals
 SET species = 'digimon'
 WHERE name like '%mon';
---  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species
 -- ----+---------+---------------+-----------------+----------+-----------+---------
---   3 | Pikachu | 2021-01-07    |               1 | f        |     13.04 | 
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     13.04 |
 --   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 | digimon
 --   2 | Gabumon | 2018-11-15    |               2 | t        |         8 | digimon
 --   4 | Devimon | 2017-05-12    |               5 | t        |        11 | digimon
@@ -43,7 +43,7 @@ WHERE name like '%mon';
 UPDATE animals
 SET species = 'pokemon'
 WHERE species IS NULL;
---  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species
 -- ----+---------+---------------+-----------------+----------+-----------+---------
 --   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 | digimon
 --   2 | Gabumon | 2018-11-15    |               2 | t        |         8 | digimon
@@ -352,5 +352,33 @@ VALUES
      '2021-01-11');
 
 
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (fullname, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
 
 
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (fullname, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (fullname, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (fullname, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (fullname, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
